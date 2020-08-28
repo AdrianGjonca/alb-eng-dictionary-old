0fce668a44ac5ae838a293613797bcf50c29c60b
Adrian Gjonca <gjoncaadrian@gmail.com>
2020-08-28 18:14:14 +0100

    Set up basic dictionary functionality without links to the page

--- src/app/dictionary/dictionary.component.ts
+++ src/app/dictionary/dictionary.component.ts
@@ -9,7 +9,9 @@ import { RecieveDefinitionsService } from '../recieve-definitions/recieve-defini
 })
 export class DictionaryComponent implements OnInit {
   word: string;
-  description: string;
+  card1: string;
+  card2: string;
+
   constructor(private _Activatedroute: ActivatedRoute, private _defService: RecieveDefinitionsService) {
     this._Activatedroute.paramMap.subscribe(params => {
       this.word = params.get('word');
@@ -19,5 +21,13 @@ export class DictionaryComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onRouted(): void {}
+  onRouted(): void {
+    let alb = this._defService.checkDataForAlbanian(this.word);
+    let eng = this._defService.checkDataForEnglish(this.word);
+    if (alb == null) alb = '"<u>' + this.word + '" does not have any registered definitions in Albanian. </u>';
+    if (eng == null) eng = '"<u>' + this.word + '" does not have any registered definitions in English. </u>';
+
+    this.card1 = alb;
+    this.card2 = eng;
+  }
 }
--- src/app/recieve-definitions/recieve-definitions.service.ts
+++ src/app/recieve-definitions/recieve-definitions.service.ts
@@ -4,6 +4,41 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecieveDefinitionsService {
+  constructor() {}
 
-  constructor() { }
+  public checkDataForEnglish(input: string): string {
+    for (var x = 0; x < this.english_def.length; x++) {
+      console.log(x);
+      if (this.english_def[x][0] == input) {
+        return this.english_def[x][1];
+      }
+    }
+    return null;
+  }
+  public checkDataForAlbanian(input: string): string {
+    for (var x = 0; x < this.english_def.length; x++) {
+      if (this.albanian_def[x][0] == input) {
+        return this.albanian_def[x][1];
+      }
+    }
+    return null;
+  }
+
+  private albanian_def = [
+    ['njereze', '<p>In English: people.</p><p>(plural)</p><p>An bipedal primate with a common ancestor with chimpanzees.</p>'],
+    ['shendet', '<p>In English: health.</p><p>The well-functioning of our bodies.</p>'],
+    [
+      'qef',
+      "<p>In English: fun.</p><p>A word that encompasses enjoyable activates.</p><p>Note: In Albanian you don't have fun, you <i>do fun</i>.</p>"
+    ]
+  ];
+  1;
+  private english_def = [
+    ['person', '<p>In Albanian: njeri.</p><p>An bipedal primate with a common ancestor with chimpanzees.</p>'],
+    ['health', '<p>In Albanian: shendet.</p><p>The well-functioning of our bodies.</p>'],
+    [
+      'fun',
+      "<p>In English: fun.</p><p>A word that encompasses enjoyable activates.</p><p>Note: In Albanian you don't have fun, you <i>do fun</i>.</p>"
+    ]
+  ];
 }
